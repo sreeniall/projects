d0058e89ad5eb66f0c97d7dc393bcd9596185355
Sravani <sravani.padarthi241@gmail.com>
2017-08-28 20:26:57 +0200

    Tech-1234NewUtil additions

---
+++ BookStore/src/main/java/com/samples/suj/StoreUtil.java
@@ -0,0 +1,14 @@
+package com.samples.suj;
+
+public class StoreUtil {
+
+	public static String getName(String nameAndNumber) {
+		return nameAndNumber.substring(2, 8);
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(StoreUtil.getName("12sreenivas566"));
+		
+	}
+}
